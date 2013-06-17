9721994ce67db5d373c6afc8e6c5dfc93043c3da
amferret <derp>
2013-06-17 15:52:11 -0700

    Derp...

--- testcombineargs.c
+++
@@ -1,9 +0,0 @@
-#include "combineargs.h"
-
-#include <stdio.h>
-
-void main(void) {
-    printf("Test zero: %s\n",combineargs(23,"one","twooo","three",NULL));
-    printf("Test one: %s\n",combineargs(23,"one","twooo","three",NULL));
-    printf("Test two: %s\n",combineargs(23,"one","twooo","three",NULL));
-}
