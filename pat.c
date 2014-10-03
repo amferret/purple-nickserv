8ba0d1b787ce2f3369836dd4a45e983295ce71c1
user <nobody>
2014-10-03 12:14:05 -0700

    auto 26 61 513

--- pat.c
+++ pat.c
@@ -1,22 +1,30 @@
 #include "pat.h"
 
 gboolean pat_setup(struct pat* self, const char* pattern) {
-  g_pats.ask_for_register.pat = pcre_compile(pattern,0,
+  self->pat = pcre_compile(pattern,0,
 				  &err,&erroffset,NULL);
-  if(!g_pats.ask_for_register.pat) {
+  if(!self->pat) {
     fprintf(stderr,"PCRE COMPILE ERROR %s\n",err);
     return FALSE
   }
 
-  g_pats.ask_for_register.study = pcre_study(g_pats.ask_for_register.pat,0,&err);
+  self->study = pcre_study(g_pats.ask_for_register.pat,0,&err);
   if(err) {
     fprintf(stderr,"Eh, study failed. %s\n",err);
-    return FALSE;
   }
 
   return TRUE;
 }
+
 void pat_cleanup(struct pat* self) {
+  if(self->pat) 
+    pcre_free(self->pat);
+    self->pat = NULL;
+  }
+  if(self->study) {
+    pcre_free(self->study);
+    self->study = NULL;
+  }
 }
 gboolean pat_check(struct pat* self, const char* test) {
 }
